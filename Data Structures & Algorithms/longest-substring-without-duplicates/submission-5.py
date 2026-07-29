51435064f4b875f087d2755a666e94803596a26b
robodo2020 <64770199+robodo2020@users.noreply.github.com>
2026-07-29 01:14:12 -0700

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.py
@@ -0,0 +1,34 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        abcabcbb
+        |
+          |
+        """
+        l = 0
+        res = 0
+        seen = set()
+        for r in range(len(s)):
+            while s[r] in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(s[r])
+            res = max(res, r - l + 1)
+        return res
+
+
+        """
+        key part:
+        remove left until right is not in seen
+        """
+        seen = set()
+        l = 0
+        res = 0
+        for r in range(len(s)):
+            while s[r] in seen:
+                seen.remove(s[l])
+                l += 1
+            seen.add(s[r])
+            res = max(res, r - l + 1)
+        return res
+        
